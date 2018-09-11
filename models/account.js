099934a7ce7efe54948e80909738e28b7491b09c
Rey Fuertes <reynelfuertes@gmail.com>
2018-09-12 06:39:09 +0800

    add account model

---
+++ models/account.js
@@ -0,0 +1,14 @@
+var account = (account_id, firstname, lastname, contact_number, birthday, valid_id, valid_id_number, address, created, modified) => {
+  this.account_id = account_id || null;
+  this.firstname = firstname || null;
+  this.lastname = lastname || null;
+  this.contact_number = contact_number || null;
+  this.birthday =  valid_id || null;
+  this.valid_id = valid_id || null;
+  this.valid_id_number = valid_id_number || null;
+  this.address = address || null;
+  this.created = created || null;
+  this.modified = modified || null;
+};
+
+module.exports = account;
--- routes/accounts_routes.js
+++ routes/accounts_routes.js
@@ -1,4 +1,5 @@
 var dac = require("../db/dac");
+var account = require("../models/account");
 var response = require("../config/resMsg");
 var express = require("express");
 var dateFormat = require("dateformat");
@@ -8,42 +9,82 @@ router = express.Router();
 
 var accounts = {
   list: (req, res) => {
-    dac.query(`SELECT account_id, firstname, lastname, contact_number, birthday, valid_id, valid_id_number, address, created FROM accounts`, [], function (err, data) {
-
-      if (err) {
-         res.status(401);
-         res.json(messages.ErrorResponse);
-         return;
+    dac.query(
+      `SELECT account_id, firstname, lastname, contact_number, birthday, valid_id, valid_id_number, address, created FROM accounts`,
+      [],
+      function(err, data) {
+        if (err) {
+          res.status(401);
+          res.json(messages.ErrorResponse);
+          return;
+        }
+        res.status(200);
+        res.json({ success: true, accounts: data });
+        return;
       }
-      res.status(200);
-      res.json({'success': true, 'accounts': data});
-      return;
-   });
+    );
   },
   new: (req, res) => {
-    var values = {
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      contact_number: req.body.contact_number,
-      birthday: dateFormat(req.body.birthday, "yyyy-mm-dd"),
-      valid_id: req.body.valid_id,
-      valid_id_number: req.body.valid_id_number,
-      address: req.body.address,
-      created: dateFormat(now, "yyyy-mm-dd")
-    };
+    account.firstname = req.body.firstname;
+    account.lastname = req.body.lastname;
+    account.contact_number = req.body.contact_number;
+    account.birthday = dateFormat(req.body.birthday, "yyyy-mm-dd");
+    account.valid_id = req.body.valid_id;
+    account.valid_id_number = req.body.valid_id_number;
+    account.address = req.body.address;
+    account.created = dateFormat(now, "yyyy-mm-dd hh:mm:ss");
 
     dac.query(
       `INSERT INTO accounts (firstname, lastname, contact_number, birthday, valid_id, valid_id_number, address, created) 
             VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
       [
-        values.firstname,
-        values.lastname,
-        values.contact_number,
-        values.birthday,
-        values.valid_id,
-        values.valid_id_number,
-        values.address,
-        values.created
+        account.firstname,
+        account.lastname,
+        account.contact_number,
+        account.birthday,
+        account.valid_id,
+        account.valid_id_number,
+        account.address,
+        account.created
+      ],
+      function(err, data) {
+        //catch error
+        if (err) {
+          res.status(401);
+          res.json(messages.ErrorResponse);
+          return;
+        }
+
+        //get updated accounts
+        res.status(200);
+        accounts.list(req, res);
+      }
+    );
+  },
+  update: (req, res) => {
+    account.account_id = req.body.account_id;
+    account.firstname = req.body.firstname;
+    account.lastname = req.body.lastname;
+    account.contact_number = req.body.contact_number;
+    account.birthday = dateFormat(req.body.birthday, "yyyy-mm-dd");
+    account.valid_id = req.body.valid_id;
+    account.valid_id_number = req.body.valid_id_number;
+    account.address = req.body.address;
+    account.modified = dateFormat(now, "yyyy-mm-dd");
+
+    dac.query(
+      `UPDATE accounts SET firstname = ?, lastname = ?, contact_number = ?, birthday = ?, valid_id = ?, valid_id_number = ?, address = ?, modified = ? 
+            WHERE account_id = ?`,
+      [
+        account.firstname,
+        account.lastname,
+        account.contact_number,
+        account.birthday,
+        account.valid_id,
+        account.valid_id_number,
+        account.address,
+        account.created,
+        account.account_id
       ],
       function(err, data) {
         //catch error
