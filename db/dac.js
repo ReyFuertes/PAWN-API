393197ea1c8eaa779a4511835ac9c1199debcf3a
RFuertes <reynelfuertes@gmail.com>
2019-05-19 11:41:54 +0800

    change serve creds

--- db/dac.js
+++ db/dac.js
@@ -1,24 +1,24 @@
 var mysql = require('mysql');
 
-// var pool = mysql.createPool({
-//     connectionLimit: 100, //important
-//     host: '198.57.247.137',
-//     user: 'bcss_p_u',
-//     password: 'p@55w0rd',
-//     database: 'bcss_pawnapp',
-//     port: '3306',
-//     debug: false
-// });
-
 var pool = mysql.createPool({
-  connectionLimit: 100, //important
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'pawnapp',
-  port: '3306',
-  debug: false
+    connectionLimit: 100, //important
+    host: '198.57.247.137',
+    user: 'bcss_p_u',
+    password: 'p@55w0rd',
+    database: 'bcss_pawnapp',
+    port: '3306',
+    debug: false
 });
+
+// var pool = mysql.createPool({
+//   connectionLimit: 100, //important
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'pawnapp',
+//   port: '3306',
+//   debug: false
+// });
 
 function query(sql, arg, callbackFn) {
     pool.getConnection(function (err, connection) {
